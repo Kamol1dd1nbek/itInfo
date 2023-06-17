53555bf34eaa1ff97214cc95cff1dd5dbb1cba6c
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-06-17 19:06:46 +0500

    Changes have been made to the app.js file

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
+const chalk = require("chalk");
+const log = console.log;
 const app = express();
 
+app.use(express.json());
+async function start() {
+    try {
+        await mongoose.connect(config.get("dbUri"));
+        const PORT = config.get("port") || 3030;
+        app.listen(PORT, () => {
+            log(chalk.bgGreen(chalk.black(` Server is running on port: ${PORT} `)));
+
+        })
+    } catch (error) {
+        log(chalk.bgRed("Serverda xatolik >>> "), error);
+    }
+}
+start();
